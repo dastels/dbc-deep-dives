248507a1f5bfd22d586c85f1a016d6690910f3ff
Dave Astels <dastels@icloud.com>
2014-09-21 21:01:22 -0500

    initial pass at find

--- DataStructures/lib/dbc_binary_tree.rb
+++ DataStructures/lib/dbc_binary_tree.rb
@@ -7,8 +7,20 @@ class DBCBinaryTree
   def insert(n)
     if @root.nil?
       @root = BinaryTreeNode.new(n)
+    else
+      @root.insert(n)
     end
   end
+
+
+  def find(n)
+    if @root.nil?
+      false
+    else
+      @root.find(n)
+    end
+  end
+  
 
   def to_s
     return '[]' if @root.nil?
@@ -27,9 +39,29 @@ class BinaryTreeNode
     @right = nil
   end
 
+
+  def insert(n)
+    return if n == @value
+    if n < @value
+      @left = @left.nil? ? BinaryTreeNode.new(n) : @left.insert(n)
+    else
+      @right = @right.nil? ? BinaryTreeNode.new(n) : @right.insert(n)
+    end
+    self
+  end
+
+
+  def find(n)
+    return true if @value == n
+    return @left.find(n) unless @left.nil? || n > @value
+    return @right.find(n) unless @right.nil?
+    false
+  end
+  
+
   def to_s
-    left_string = @left.nil? ? '[]' : "[#{@left.to_s}]"
-    right_string = @right.nil? ? '[]' : "[#{@right.to_s}]"
+    left_string = @left.nil? ? '-' : @left.to_s
+    right_string = @right.nil? ? '-' : @right.to_s
     "[#{left_string} #{@value} #{right_string}]"
   end
   
--- DataStructures/spec/binary_tree_spec.rb
+++ DataStructures/spec/binary_tree_spec.rb
@@ -15,7 +15,7 @@ describe 'DBCBinaryTree' do
     
     it 'should have the first insertion set the root' do
       @tree.insert(10)
-      expect(@tree.to_s).to eql('[[] 10 []]')
+      expect(@tree.to_s).to eql('[- 10 -]')
     end
     
   end
@@ -26,7 +26,41 @@ describe 'DBCBinaryTree' do
       @tree.insert(10)
     end
 
-    it 'should add 
+    it 'can add a number on the left' do
+      @tree.insert(5)
+      expect(@tree.to_s).to eql('[[- 5 -] 10 -]')
+    end
+    
+    it 'can add a number on the right' do
+      @tree.insert(15)
+      expect(@tree.to_s).to eql('[- 10 [- 15 -]]')
+    end
+
+    it 'can insert several numbers' do
+      @tree.insert(5)
+      @tree.insert(15)
+      @tree.insert(7)
+      @tree.insert(11)
+      expect(@tree.to_s).to eql('[[- 5 [- 7 -]] 10 [[- 11 -] 15 -]]')
+    end
+    
   end
+
+  context 'with several values' do
+    before(:each) do
+      @tree = DBCBinaryTree.new
+      @tree.insert(10)
+      @tree.insert(5)
+      @tree.insert(15)
+      @tree.insert(7)
+      @tree.insert(11)
+    end
+
+    it 'can find in the root' do
+      expect(@tree.find(10)).to eql true
+    end
+
+  end
+
 
 end
