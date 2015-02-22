098766f92248d8584ec556a9eb8dbc695536403c
Dave Astels <dastels@iCloud.com>
2015-02-22 17:03:30 -0600

    Flesh out specs and code

--- DataStructures/lib/dbc_graph.rb
+++ DataStructures/lib/dbc_graph.rb
@@ -14,59 +14,120 @@ class DbcGraph
   end
 
   def delete(v)
+    i = find_node_helper(v)
+    @nodes.delete_at(i) unless i.nil?
   end
 
   def find(v)
-    i = @nodes.index {|n| n.value == v}
+    i = find_node_helper(v)
     return @nodes[i] unless i.nil?
     nil
   end
 
+  #  tests whether there is an edge from node x to node y.
   def adjacent?(x, y)
+    x.connected_to?(y)
   end
 
+  # lists all nodes y such that there is an edge from x to y.
   def neighbors(x)
+    x.neighbors
   end
 
+  # adds the edge from x to y, if it is not there.
   def connect(x, y)
+    x.connect_to(y)
   end
 
+  # removes the edge from x to y, if it is there.
   def disconnect(x, y)
+    x.disconnect_from(y)
   end
 
+  # returns the value associated with the node x.
   def get_node_value(x)
+    x.value
   end
 
+  # sets the value associated with the node x to a.
   def set_node_value(x, v)
+    x.value = v
   end
 
+  # returns the value associated to the edge (x,y).
   def get_edge_value(x, y)
+    e = x.connection_to(y)
+    e.nil? ? nil : e.value
   end
 
+  # sets the value associated to the edge (x,y) to v.
   def set_edge_value(x, y, v)
+    e = x.connection_to(y)
+    e.value = v unless e.nil?
   end
-  
-end
 
+  private
 
-class DbcGraphNode
-  attr_reader :value
-  
-  def initialize(v=nil)
-    @value = v
-    @outgoing = []
+  def find_node_helper(v)
+    @nodes.index {|n| n.value == v}
   end
   
 
-end
+  class DbcGraphNode
 
+    attr_accessor :value
+    
+    def initialize(v=nil)
+      @value = v
+      @outgoing = []
+    end
+    
+    def connect_to(a_node)
+      unless connected_to?(a_node)
+        e = DbcGraphEdge.new(self, a_node)
+        @outgoing << e
+        e
+      end
+    end
+    
+    def disconnect_from(a_node)
+      i = @outgoing.index {|e| e.goes_to?(a_node)}
+      @outgoing.delete_at(i) unless i.nil?
+    end
+    
+    def connected_to?(a_node)
+      i = connection_to(a_node)
+      !i.nil?
+    end
+    
+    def neighbors
+      @outgoing.map {|e| e.goes_where}
+    end
 
-class DbcGraphEdge
-
-  def initilize(source, destination)
-    @source = source
-    @destination = destination
-    @value = nil
+    def connection_to(a_node)
+      i = @outgoing.index {|e| e.goes_to?(a_node)}
+      i.nil? ? nil : @outgoing[i]
+    end
   end
   
+  
+  class DbcGraphEdge
+
+    attr_accessor :value
+    
+    def initialize(source, destination, value=nil)
+      @source = source
+      @destination = destination
+      @value = value
+    end
+    
+    def goes_to?(a_node)
+      @destination == a_node
+    end
+    
+    def goes_where
+      @destination
+    end
+
+  end
 end
--- DataStructures/spec/graph_spec.rb
+++ DataStructures/spec/graph_spec.rb
@@ -20,4 +20,111 @@ describe 'A DbcGraph' do
     end
   end
 
+  context 'with a node' do
+    before(:each) do
+      @dave = @graph.add("Dave")
+    end
+    
+    it 'can delete a node' do
+      @graph.delete("Dave")
+      expect(@graph.find("Dave")).to be_nil # prove it's not there
+    end
+
+    it 'can fetch the value of a node' do
+      expect(@graph.get_node_value(@dave)).to eq "Dave"
+    end
+    
+    it 'can change the value of a node' do
+      @graph.set_node_value(@dave, "David")
+      expect(@graph.get_node_value(@dave)).to eq "David"
+      expect(@graph.find("Dave")).to be_nil
+      expect(@graph.find("David")).to eq @dave
+    end
+
+  end
+
+  context 'with two unconnected nodes' do
+    before(:each) do
+      @dave = @graph.add("Dave")
+      @apple = @graph.add("Apple")
+    end
+
+    it 'knows they are not adjacent' do
+      expect(@graph.adjacent?(@dave, @apple)).to eq false
+    end
+
+    it 'knows that neither have neighbors' do
+      expect(@graph.neighbors(@dave)).to be_empty
+      expect(@graph.neighbors(@apple)).to be_empty
+    end
+
+    it 'can connect nodes' do
+      @graph.connect(@dave, @apple)
+      expect(@graph.adjacent?(@dave, @apple)).to eq true
+    end
+
+    it 'supports unlabeled connections' do
+      @edge = @graph.connect(@dave, @apple)      
+      expect(@graph.get_edge_value(@dave, @apple)).to be_nil
+    end
+
+    it 'supports changing the label of connections' do
+      @edge = @graph.connect(@dave, @apple)
+      @graph.set_edge_value(@dave, @apple, "buys")
+      expect(@graph.get_edge_value(@dave, @apple)).to eq "buys"
+    end
+
+  end
+
+  context 'with two connected nodes' do
+    before(:each) do
+      @dave = @graph.add("Dave")
+      @apple = @graph.add("Apple")
+      @edge = @graph.connect(@dave, @apple)
+    end
+
+    it 'knows adjacency is directed' do
+      expect(@graph.adjacent?(@dave, @apple)).to eq true
+      expect(@graph.adjacent?(@apple, @dave)).to eq false
+    end
+
+    it 'knows neighborship is directed' do
+      expect(@graph.neighbors(@dave)).to eq [@apple]
+      expect(@graph.neighbors(@apple)).to be_empty
+    end
+
+    it 'can disconnect nodes' do
+      @graph.disconnect(@dave, @apple)
+      expect(@graph.adjacent?(@dave, @apple)).to eq false
+    end
+  end
+
+
+  
 end
+
+
+# BONUS
+#
+# 1. Add support for proving edge labels to all related calls: connect, disconnect
+# adjacent?
+#
+# 2. Change neighbors to return an array of pairs (connected-node,
+# edge-label). That tells us not only what is connected, but how.
+#
+# 3. Add a path?(x, y) method that returns whether a path exists from
+# x to y
+#
+# 4. Add a path(x, y) method that returns an array of the edge labels
+# and node values for a path from x to y:
+#  [x, e1, n1, e2, n2 ... em, y]
+#
+# 5. Make a graph with nodes of people and movies.
+#    Connect the nodes with the following relationships:
+#      person --friend_of--> person
+#      person --likes--> movie
+#      person --dislikes--> movie
+#
+#   Write code to start with a person node i.e. "you", and find movies
+#   to watch: movies that at least half of your friends like and less
+#   than quarter of them dislike.
